5fdbbf7cd3e0e6c8f38a8098963e9499f28522b0
Rust Glomba de Souza <rust.glomba@gmail.com>
2022-12-30 00:33:29 -0300

    Atualização para pausa no projeto

--- src/atividades/exercicio2.rs
+++ src/atividades/exercicio2.rs
@@ -1,8 +1,15 @@
-use chrono::Local;
+//Modulo para data
+
+use chrono::prelude::*;
 
 pub fn data_atual() {
     let t = Local::now();
-    println!("O exercicio2 exibe a data atual no formato ISO 8601.");
-    println!("Agora veremos a data atual:");
-    println!("{}", t);
+    let ano: i32 = t.year();
+    let mes: u32 = t.month();
+    let dia: u32 = t.day();
+    let hora: u32 = t.hour();
+    let minuto: u32 = t.minute();
+
+    println!("O exercicio2 exibe a data atual.");
+    println!("Hoje é {}-{}-{}, a hora é {}:{}", dia, mes, ano, hora, minuto);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod atividades;
 use atividades::exercicio1::saudacao;
 use atividades::exercicio2::data_atual;
+
 fn main() {
     saudacao();
     data_atual();
+
+    println!("Fim dos exercícios.");
 }
