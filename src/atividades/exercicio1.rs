91ed870dbacf926cf08bf446a38b53111e2916ba
Rust Glomba de Souza <rust.glomba@gmail.com>
2022-12-29 21:40:38 -0300

    Usado fmt no arquivo main

--- src/atividades/exercicio1.rs
+++ src/atividades/exercicio1.rs
@@ -1,6 +1,5 @@
 // Neste exercício é criado uma função que recebe um entrada do usuário que será usada como seu nome.
 
-
 use std::io;
 
 pub fn saudacao() {
@@ -8,7 +7,7 @@ pub fn saudacao() {
     let mut nome = String::new(); // Varíavel mutável para receber a entrada
     println!("O exercício1 é um programa de saudação.");
     println!("Me informe seu nome e irei cumprimentá-lo.");
-    
+
     io::stdin().read_line(&mut nome).unwrap(); // Faz a leitura da entrada
 
     println!("{} {}", greeting, nome); // Imprime a saudação com o nome inserido.
--- src/atividades/exercicio2.rs
+++ src/atividades/exercicio2.rs
@@ -4,5 +4,5 @@ pub fn data_atual() {
     let t = Local::now();
     println!("O exercicio2 exibe a data atual no formato ISO 8601.");
     println!("Agora veremos a data atual:");
-    println!("{}",t);
+    println!("{}", t);
 }
